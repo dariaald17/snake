04755e3953015265f42e0217924742ee540c276e
Aldokhina Daria <darya.aldokhina@mail.ru>
2022-12-02 13:34:32 +0300

    add start game by click on start button

--- main.js
+++ main.js
@@ -5,15 +5,33 @@ const COLUMN_NUMBER = 10;
 
 const grid = document.querySelector('.game-grid')
 const btnAction = document.getElementById('action-button')
+document.getElementById('action-button').addEventListener('click', () =>{
+   tick();
+})
+
+const gameStatuses = {
+  init: 'Init',
+  play: 'Play',
+  pause: 'Pause',
+  end: 'End',
+}
+
+const directions = {
+  up: 'up',
+  down: 'down',
+  left: 'left',
+  right: 'right',
+}
 
 const state = {
+  status: gameStatuses.play,
   cells: [],
+  score: 0,
   snake: {
-    direction: [0, 1],
-    coords: [[5, 2], [5, 3], [5, 4]],
+    direction: directions.up,
+    coords: [[5, 7], [5, 8], [5, 9]],
   },
-  fruitPosition: null,
-  fruit: null
+  fruit: null,
 };
 
 function getRandomInt(min, max) {
@@ -32,7 +50,7 @@ function generateGrid() {
         element,
         rowIndex, columnIndex,
         isSnake: false,
-        isFruit: '',
+        fruit: '',
       }
       state.cells.push(cell);
     }
@@ -52,11 +70,8 @@ const render = () => {
   state.cells.forEach((cell) => {
     cell.element.className = 'cell';
     if (cell.isSnake) cell.element.classList.add('snake');
-    else if (cell.isFruit !== '') cell.element.classList.add(cell.isFruit);
+    else if (cell.fruit !== '') cell.element.classList.add(cell.fruit);
   })
-}
-
-const renderSnake = () => {
 }
 
 const generateFruit = () => {
@@ -67,51 +82,123 @@ const generateFruit = () => {
   while (isInvalidPoint) {
     columnIndex = getRandomInt(0, COLUMN_NUMBER);
     rowIndex = getRandomInt(0, ROW_NUMBER);
-  
+
     isInvalidPoint = state.snake.coords.some((coord) => coord[0] === columnIndex || coord[1] === rowIndex);
   }
 
-  state.cells[rowIndex * COLUMN_NUMBER + columnIndex].isFruit = getFruitClass();
+  state.cells[rowIndex * COLUMN_NUMBER + columnIndex].fruit = getFruitClass();
 }
 
 function getFruitClass() {
   const classes = ['apple', 'orange', 'grap']
-  const index = Math.round(Math.random() * (classes.length - 1))
+  const index = getRandomInt(0, classes.length - 1)
   return 'game-item--' + classes[index]
 }
 
 const moveSnake = () => {
-  state.snake.coords.forEach((coord) => {
-    coord[0] += state.snake.direction[0];
-    coord[1] += state.snake.direction[1];
+  const coords = state.snake.coords
+
+  let next = [...coords[0]]
+  const last = [...coords[coords.length - 1]]
+
+  const isFruitEaten = moveSnakeHead()
+
+  state.snake.coords.forEach((coord, index) => {
+    if (index > 0) {
+      const temp = [...coords[index]]
+      coords[index] = next
+      next = temp
+    }
   });
+
+  if (isFruitEaten) {
+    generateFruit()
+    coords.push(last)
+  }
 }
 
+const moveSnakeHead = () => {
+  let isFruitEaten = false
+  const xCoordinate = state.snake.coords[0][0]
+  const yCoordinate = state.snake.coords[0][1]
+
+  switch (state.snake.direction) {
+    case directions.up:
+      state.snake.coords[0][1] = yCoordinate - 1
+      break
+    case directions.down:
+      state.snake.coords[0][1] = yCoordinate + 1
+      break
+    case directions.left:
+      state.snake.coords[0][0] = xCoordinate - 1
+      break
+    case directions.right:
+      state.snake.coords[0][0] = xCoordinate + 1
+      break
+  }
+
+  placeSnake()
+
+  state.cells.forEach((cell) => {
+    if (cell.isSnake && cell.fruit) {
+      state.score++
+      document.getElementById('score').innerHTML=state.score;
+      cell.fruit = ''
+      isFruitEaten = true
+    }
+  })
+
+  return isFruitEaten
+}
+
+
+
 const tick = () => {
-  moveSnake();
-  placeSnake();
-  render();
-  setTimeout(tick, 1000);
+  if (state.status === gameStatuses.play) {
+    moveSnake();
+     placeSnake();
+    render();
+  }
+  setTimeout(tick, 300);
 }
 
 const main = () => {
   generateGrid();
   generateFruit();
-  tick();
+  //tick();
 }
 
 main()
 
+const changeGameStatus = (status) => {
+  if (status === gameStatuses.pause) {
+    if (state.status === gameStatuses.pause) {
+      state.status = gameStatuses.play
+    }
+    else if (state.status === gameStatuses.play) {
+      state.status = gameStatuses.pause
+    }
+  }
+}
+
 document.addEventListener('keydown', (event) => {
-  if (event.code === 'ArrowUp') {
-    state.snake.direction = [0, 1];
-    state.snake.direction[0] = 0;
-    state.snake.direction[1] = -1;
-console.log(45678);
-}
-else if (event.code === 'ArrowDown') {
-  state.snake.direction = [0,-1];
-  state.snake.direction[0] = 0;
-  state.snake.direction[1] = 1;
-}
+  const current = state.snake.direction
+
+  if (event.code === 'Space') {
+    changeGameStatus(gameStatuses.pause)
+  }
+
+  if (event.code === 'ArrowUp' && current !== directions.down) {
+    state.snake.direction = directions.up
+  }
+  else if (event.code === 'ArrowDown' && current !== directions.up) {
+    state.snake.direction = directions.down
+  }
+  else if (event.code === 'ArrowLeft' && current !== directions.right) {
+    state.snake.direction = directions.left
+  }
+  else if (event.code === 'ArrowRight' && current !== directions.left) {
+    state.snake.direction = directions.right
+  }
 })
+document.addEventListener('')
